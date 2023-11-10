98bd4e0fbf496e75ec96300517b907bbf296b25b
rlscott <rebekah.lynne.scott@gmail.com>
2023-11-10 11:27:45 -0600

    update dataset

--- code/clean_data.R
+++ code/clean_data.R
@@ -5,10 +5,24 @@ border_raw <- readr::read_csv(file = "data/raw_border_crossing_entry_data_11_9_2
 # clean it
 # (to do but at least we thought about it!)
 
-border_clean <- border_raw
+library(tidyverse)
+border_clean <- border_raw %>%   mutate(
+  date_long = lubridate::my(Date),
+  Month = month(date_long, label = TRUE),
+  Year = year(date_long), 
+  Port_name = `Port Name`, 
+  Port_code = `Port Code`, 
+)
 
-# save it
-border <- border_clean
+
+head(border_clean)
+
+# save it (with some selected columns)
+border <- border_clean %>% select(Port_name, State, Port_code, Border, Date, 
+                                  Month, Year, Measure, Value, Latitude, 
+                                  Longitude, Point)
+
+head(border)
 save(file = "data/border_clean.Rdata", border)
 
 # load it (for future use)
