ddf6ff9611fe32bf3cf4af4f30a4f6b5ce8f098c
rlscott <rebekah.lynne.scott@gmail.com>
2023-11-10 12:36:49 -0600

    more updated dataset 2.0

--- code/clean_data.R
+++ code/clean_data.R
@@ -10,21 +10,27 @@ border_clean <- border_raw %>%   mutate(
   date_long = lubridate::my(Date),
   Month = month(date_long, label = TRUE),
   Year = year(date_long), 
-  Port_name = `Port Name`, 
-  Port_code = `Port Code`, 
-)
+  Port_name = as.factor(`Port Name`), 
+  Port_code = as.factor(`Port Code`), 
+  State = as.factor(State), 
+  Border = as.factor(Border), 
+  Measure = as.factor(Measure), 
+  Type = as.factor(ifelse(
+    Measure %in% c('Trains','Buses',"Personal Vehicles","Rail Containers Empty",
+                   "Truck Containers Loaded","Rail Containers Loaded","Trucks",
+                   "Truck Containers Empty"),"Object","People"))
+  ) 
 
-
-head(border_clean)
+# head(border_clean)
 
 # save it (with some selected columns)
 border <- border_clean %>% select(Port_name, State, Port_code, Border, Date, 
-                                  Month, Year, Measure, Value, Latitude, 
+                                  Month, Year, Measure, Value, Type, Latitude, 
                                   Longitude, Point)
 
-head(border)
+# head(border)
 save(file = "data/border_clean.Rdata", border)
 
 # load it (for future use)
-load("data/border_clean.Rdata")
+# load("data/border_clean.Rdata")
 
