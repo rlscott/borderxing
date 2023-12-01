09c35dfaf3786ecc8085cba98ada968d6fc11165
rlscott <rebekah.lynne.scott@gmail.com>
2023-11-30 20:47:20 -0600

    r writeup draft

--- code/rebekah/multinomial.R
+++ code/rebekah/multinomial.R
@@ -3,6 +3,57 @@
 library(tidyverse)
 load("data/border_clean.Rdata")
 
+# revamped multinomail stuff --------------
+
+# get the total counts of each measure by each port 
+border_multinom_long <- border %>% 
+  # first get the sum of counts by measure and port 
+  mutate(measure_count_by_port = sum(Value), .by = c(Measure, Port_code)) %>%
+  # next get the sum of all measure  counts by port
+  mutate(total_count_by_port = sum(Value), .by = Port_code) %>% 
+  # create a proportion of the two 
+  mutate(measure_porp = measure_count_by_port / total_count_by_port) %>% 
+  # select what you need 
+  select(Port_code, Measure, measure_porp, measure_count_by_port, total_count_by_port) %>% 
+  # get the unique values and arrange by port code (to look pretty)
+  unique() %>% arrange(Port_code, Measure) %>% 
+  # select what you need 
+  select(Port_code, Measure, measure_count_by_port)
+
+# make it wide 
+border_multinom_wide <- border_multinom_long %>%
+  pivot_wider(names_from = Measure, values_from = measure_count_by_port)
+
+# get the average and median mult distn... 
+aggregate_multinom_long <- border %>% 
+  # calculate the average and median count by each measure
+  mutate(average_measures = mean(Value), middle_measures = median(Value), .by = Measure) %>% 
+  # select important variables and select the unique values
+  select(Measure, average_measures, middle_measures) %>% unique() %>% 
+  # calculate the porportions of each measure (for both the average and median measures)
+  mutate(total_average_measures = sum(average_measures), 
+         total_middle_measures = sum(middle_measures), 
+         avg_measure_porp = average_measures / total_average_measures, 
+         med_measure_porp = middle_measures / total_middle_measures) %>% 
+  arrange(Measure) %>% select(Measure, avg_measure_porp, med_measure_porp)
+
+# individual test 
+chisq.test(border_multinom_wide[2, -1], p = aggregate_multinom_long$avg_measure_porp)$p.value
+
+# dosen't work since they don't have complete cases :(
+apply(border_multinom_wide[-1], 1, function(x) chisq.test(x, p =aggregate_multinom_long$avg_measure_porp)$p.value)
+
+# 4 ports with incomplete cases 
+border_multinom_wide %>% filter(!complete.cases(border_multinom_wide)) 
+
+# get the complete cases 
+complete_border_multinom_wide <- border_multinom_wide %>% filter(complete.cases(border_multinom_wide))
+
+apply(complete_border_multinom_wide[-1], 1, function(x) 
+  chisq.test(x, p =aggregate_multinom_long$med_measure_porp, simulate.p.value = TRUE)$p.value)
+
+# old work --------------
+# the giant graph hehe
 border %>% # filter(Border == "US-Mexico Border") %>% 
   ggplot(aes(x = reorder(Port_name, as.numeric(Measure), na.rm = TRUE), fill = Measure)) + 
   geom_bar(position = "fill") + coord_flip() + xlab("Port Name") + 
@@ -96,35 +147,22 @@ chisq.test(yes,  border_aggregate_multinom$avg_measure_porp)$p.value
 
 
 
+# looking at the states --------------
+border %>% select(Port_name, State) %>% unique() %>% count(State) %>% arrange(-n)
+
+border %>% select(Port_name, State, Border) %>% unique() %>% 
+  ggplot(aes(x = fct_infreq(State), 
+             #fill = Border
+             )) + 
+  geom_bar(stat = "count") + coord_flip() + 
+  ylab("Ports of Entry Count") + xlab("State") + 
+  ggtitle("Port of Entry Counts by State")
+
+border %>% select(Port_name, State, Border) %>% unique() %>% 
+  ggplot(aes(x = fct_rev(fct_infreq(State)))) + geom_bar(stat = "count") + coord_flip() + 
+  ylab("Ports of Entry Count")
 
 
+# border %>% select(Port_name, State) %>% unique() %>% filter(State == "North Dakota")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-border %>% filter(Port_name == "Eastport")
-
-border2 <- border %>% mutate(Port_name2 = as.factor(case_when(
-  Port_name == "Eastport" & Port_code == "3302" ~ "Eastport ID", 
-  Port_name == "Eastport" & Port_code == "0103" ~ "Eastport ME", 
-  .default = Port_name)))
-
-border %>% select(Port_name, Port_code, State) %>% 
-  filter(Port_name == "Eastport") %>% unique() %>% arrange(Port_name) 
-
-border2 %>% select(Port_name, Port_name2, Port_code, State) %>% 
-  filter(Port_code %in% c("3302","0103")) %>% unique()
-
-border2 %>% select(Port_name2, Port_name, Port_code, State) %>% unique()
-levels(border2$Port_name2)
--- code/rebekah/rebekah_initial.R
+++ code/rebekah/rebekah_initial.R
@@ -1,1 +1,21 @@
 load("data/border_clean.Rdata")
+
+border$Date
+min(border$Date)
+
+levels(border$Date)
+
+border %>% ggplot(aes(x = Date, y = Value, fill = Measure)) + geom_point()
+
+
+# leaflet stuffs
+
+stations <- unique(border %>% select(Port_code, Port_name, Longitude, Latitude) %>% filter(Port_name != "Anchorage"))  
+
+library(leaflet)
+
+png("code/rebekah/help.png")
+leaflet(stations) %>%
+  addTiles() %>%
+  addCircleMarkers(~Longitude, ~Latitude, popup=~Port_name, radius=3)
+dev.off()
